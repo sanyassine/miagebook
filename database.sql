c3af9e332f36990bbf1de5251f7ff165385610c3
assimtsenoussi <assim.senoussi@gmail.com>
2018-04-05 11:27:32 +0200

    working on sql database

--- database.sql
+++ database.sql
@@ -1,3 +1,8 @@
+drop table friends;
+drop table comments;
+drop table posts;
+drop table userprofiles;
+
 create table userprofiles(
 	login varchar2(255) primary key,
 	password varchar2(255),
@@ -7,16 +12,28 @@ create table userprofiles(
 );
 
 create table posts(
+    id_post integer,
 	login_author varchar2(255),
 	date_post timestamp,
+    title varchar2(255),
 	content varchar2(255),
-	constraint pk_posts primary key (login_author,date_post,content),
+	constraint pk_posts primary key (id_post),
 	constraint fk_log_author foreign key (login_author) references userprofiles(login)
+);
+
+create table comments(
+    id_comments integer,
+    id_post integer,
+    author_login varchar2(255),
+    content varchar2(255),
+    constraint comments_pk primary key(id_comments),
+    constraint author_comments_fk foreign key (author_login) references userprofiles(login)
 );
 
 create table friends(
 	login_user varchar2(255),
 	login_friend varchar2(255),
+    constraint friends_pk primary key (login_user,login_friend),
 	constraint friend_friends_fk foreign key (login_friend) references userprofiles(login),
 	constraint user_friends_fk foreign key (login_user) references userprofiles(login)
 );

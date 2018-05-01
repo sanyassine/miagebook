d3170b49e4edc5af729bfa4be69b49c16e98e870
sansaoui <yassine.sansaoui@gmail.com>
2018-05-01 16:27:41 +0200

    add @GET to resource status

--- src/main/java/resources/StatusResource.java
+++ src/main/java/resources/StatusResource.java
@@ -6,6 +6,7 @@ import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -17,6 +18,7 @@ import persistence.PostMapper;
 public class StatusResource {
 	
 	// findByIdUser
+	@GET
 	@Path("user/{loginUser}")
 	@Produces("application/json")
 	public String findByUser(@PathParam("loginUser") String loginUser) {
